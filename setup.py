2987bf599e60cd082926da687d2d89bb9a5153ef
Fokko Driesprong <fokkodriesprong@godatadriven.com>
2018-05-16 19:16:46 +0200

    Rename

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if (version_info < (2, 7)) or ((3, 0) <= version_info < (3, 4)):
 else:
     pytest_runner = 'pytest-runner'
 
-setup(name='poloniex',
+setup(name='poloniex_human',
       version='0.0.17',
       description='Poloniex API client for humans',
       long_description=long_description,
@@ -18,7 +18,7 @@ setup(name='poloniex',
       author='Enrico Bacis, Daniele Ciriello',
       author_email='enrico.bacis@gmail.com, ciriello.daniele@gmail.com',
       license='MIT',
-      packages=['poloniex'],
+      packages=['poloniex_human'],
       setup_requires=[pytest_runner],
       install_requires=['requests', 'six'],
       tests_require=['pytest', 'responses'],
